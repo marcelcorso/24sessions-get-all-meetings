4ea67125831155a4fc90d4de129f7a5259be6785
Marcel Corso <marcel@messagebird.com>
2023-08-03 11:34:26 -0300

    hello meetings

---
+++ README.md
@@ -0,0 +1,21 @@
+# fetch all meeting pages 24sessions
+
+this script does sequential requests to the "data api" https://apidocs.24sessions.com/ of 24sessions. 
+
+it fetches all meeting pages and add all meetings to one array. 
+
+# setup 
+
+```
+mv dot-env-example .env
+
+# add credentials to .env
+ 
+node install
+```
+
+# run 
+
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+require('dotenv').config()
+
+let instance = "meet"; // or supervista or ... 
+let apiDomain = `https://${instance}.api.24sessions.com`;
+
+// TODO we don't need to request this all the time. cache it until it expires
+async function getAccessToken() {
+
+  let params = {
+    "client_id": process.env.CLIENT_ID,
+    "client_secret": process.env.CLIENT_SECRET,
+    "grant_type": "client_credentials",
+  };
+
+  let query = new URLSearchParams(params).toString(); 
+	let url = apiDomain + "/oauth/v2/token?" + query; 
+
+  const resp = await fetch(url)
+    .then(response => response.json());
+
+  return resp.access_token;
+}
+
+let allMeetings = [];
+
+let page = 1;
+
+fetchData(page, allMeetings);
+
+
+async function fetchData(page, meetings) {
+
+  let pagePath = `/meetings?page=${page}`
+  let pageURL = apiDomain + pagePath;
+
+  console.log("fetching " + pageURL);
+
+  let token = await getAccessToken();
+
+  let headers = {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer ' + token, 
+  };
+
+  // Retrieve a list of all articles from Intercom
+  fetch(pageURL, { headers: headers})
+    .then(response => response.json())
+    .then(data => {
+      console.log("page", data['@id']);
+      for (const meeting of data['hydra:member']) {
+        meetings.push(meeting);
+      }
+
+/*
+  '@context': '/contexts/Meeting',
+  '@id': '/meetings',
+  '@type': 'hydra:PagedCollection',
+  'hydra:totalItems': 41,
+  'hydra:itemsPerPage': 200,
+  'hydra:firstPage': '/meetings',
+  'hydra:lastPage': '/meetings',
+  'hydra:member': [
+    {
+*/
+      // if current page is different than the last page
+      if (data['@id'] != data['hydra:lastPage']) {
+        // recurse
+        fetchData(page+1, meetings);
+      } else {
+        let meetingCount = meetings.length;
+        console.log(`we are at the last page! we got ${meetingCount} meetings in total.`)
+      }
+    })
+    .catch(error => console.error(error));
+}
+
